c3dcd80a85f2feb5cc7e41632829222f0ac3bc27
yunsu <than4502@gmail.com>
2022-12-01 16:01:04 +0900

    feat: seo 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@app/assets/styles/globals.css";
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
@@ -14,7 +15,12 @@ const queryClient = new QueryClient({
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />;
+      <DefaultSeo
+        defaultTitle="Next Shop"
+        titleTemplate="% • Next Shop"
+        description="e-commerce nextjs+reactquery+storybook playground"
+      />
+      <Component {...pageProps} />
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
